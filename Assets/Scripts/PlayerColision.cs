3068898bd72c1fd631275b74b40a95334048bf22
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-05-30 12:05:50 -0300

    Update 30/05
    Counters

---
+++ Assets/Scripts/PlayerColision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColision : MonoBehaviour
+{
+    public int OriginalPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3(0, OriginalPos, 0);
+        }
+        if (collision.gameObject.name == "DeathFloor")
+        {
+            transform.position = new Vector3(0, OriginalPos, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public Text txtTime;
+    public Text txtTimeFloored;
+    public Text txtCountDown;
+
+    public float TimeToChange;
+    public float waitTime;
+    int counter;
+
+    void Start()
+    {
+        counter = 3;
+        txtCountDown.text = counter.ToString();
+    }
+
+    void Update()
+    {
+        //Acceder al tiempo transcurrido desde el inicio del juego;
+        txtTime.text = Time.time.ToString();
+        txtTimeFloored.text = Mathf.FloorToInt(Time.time).ToString();
+
+        if (TimeToChange<Time.time)
+        {
+            counter--;
+            if (counter != 0)
+            {
+                txtCountDown.text = counter.ToString();
+                TimeToChange += waitTime;
+            }
+            else
+            {
+                txtCountDown.text = "GO!";
+            }
+        }
+    }
+}
